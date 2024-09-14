3b1bc7773a51686fc2a71493ca1ef724e66d32b9
Leandro Lima <llimasilva@proton.me>
2024-09-14 12:03:47 -0300

    adicionando configuração com banco de dados e instalando pacotes

--- Services/Mango.Services.CouponAPI/Program.cs
+++ Services/Mango.Services.CouponAPI/Program.cs
@@ -1,5 +1,10 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
