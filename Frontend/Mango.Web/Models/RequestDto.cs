556a627ee88076910856e407ab8aaffc2c20a3bd
Leandro Lima <llimasilva@proton.me>
2024-09-20 18:58:19 -0300

    criando projeto web mvc

---
+++ Frontend/Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mango.Web.Models;
+
+public class RequestDto
+{
+    public ApiType ApiType { get; set; } = ApiType.GET;
+    public string Url { get; set; }
+    public object Data { get; set; }
+    public string AccessToken { get; set}
+}
