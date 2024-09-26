948d6617dc0bc59ad633b2d013b72238c440526c
Leandro Lima <leandro.lsilva@pagar.me>
2024-09-26 17:56:22 -0300

    finishing web crud

--- Frontend/Mango.Web/Controllers/CouponController.cs
+++ Frontend/Mango.Web/Controllers/CouponController.cs
@@ -16,11 +16,53 @@ namespace Mango.Web.Controllers
         {
             List<CouponDto>? list = new();
             ResponseDto? response = await _couponSerivce.GetAllCouponAsync();
-            if(response != null && response.IsSuccess)
+            if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponSerivce.CreateCouponAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto? response = await _couponSerivce.GetCouponByIdAsync(couponId);
+            if (response != null && response.IsSuccess)
+            {
+                CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponSerivce.DeletecoupounAsync(couponDto.CouponId);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            return View(couponDto);
         }
 
     }
